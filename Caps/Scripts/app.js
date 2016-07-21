c0bbf897209acffef16244766af9c0e7bfb9ca38
JS4lcaEq <condorrr@gmail.com>
2016-07-22 00:05:44 +0300

    68678

---
+++ Caps/Scripts/app.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+
+    var elements = { body: $("body") };
+    var bussy = false;
+
+    function onScroll(scroll) {
+        if (scroll > 5) {
+            elements.body.addClass("scrolled");
+        } else {
+            elements.body.removeClass("scrolled");
+        }
+    }
+
+    $(document).on("scroll", function (e) {
+        if (bussy) {
+            return;
+        }
+        bussy = true;
+
+        var timer = setTimeout(function () {
+        var s = window.pageYOffset;
+        onScroll(s);
+        console.log("jQuery scroll: ", s);
+            bussy = false;
+        }, 50);
+
+    });
+
+    console.log("ready!");
+});
